a070220082e0f89da21be7a5599f7391d1dc1828
Bruno Borges <bborgesfilho33@gmail.com>
2023-07-20 21:17:33 -0300

    fixing dateFilter

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,47 +1,50 @@
 import { Item } from '../types/Item';
 
 export const getCurrentMonth = () => {
-	let now = new Date();
-	return `${now.getFullYear()}-${now.getMonth() + 1}`;
+  let now = new Date();
+  return `${now.getFullYear()}-${now.getMonth() + 1}`;
 };
 
 export const filterListByMonth = (list: Item[], date: string): Item[] => {
-	let newList: Item[] = [];
-	let [year, month] = date.split('-');
+  let newList: Item[] = [];
+  let [year, month] = date.split('-');
 
-	for (let i in list) {
-		if (list[i].date.getFullYear() === parseInt(year) && list[i].date.getMonth() + 1 === parseInt(month)) {
-			newList.push(list[i]);
-		}
-	}
+  for (let i in list) {
+    if (
+      list[i].date.getFullYear() === parseInt(year) &&
+      list[i].date.getMonth() + 1 === parseInt(month)
+    ) {
+      newList.push(list[i]);
+    }
+  }
 
-	return newList;
+  return newList;
 };
 
 export const dateFormatter = (date: Date): string => {
-	let year = date.getFullYear();
-	let month = date.getMonth() + 1;
-	let day = date.getDay();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
 
-	return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+  return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
 };
 const addZeroToDate = (n: number): string => (n < 10 ? `0${n}` : `${n}`);
 
 export const currentMonthFormatter = (currentMonth: string): string => {
-	let [year, month] = currentMonth.split('-');
-	let months = [
-		'Janeiro',
-		'Fevereiro',
-		'Março',
-		'Abril',
-		'Maio',
-		'Junho',
-		'Julho',
-		'Agosto',
-		'Setembro',
-		'Outubro',
-		'Novembro',
-		'Dezembro',
-	];
-	return `${months[parseInt(month) - 1]} de ${year}`;
+  let [year, month] = currentMonth.split('-');
+  let months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+  return `${months[parseInt(month) - 1]} de ${year}`;
 };
